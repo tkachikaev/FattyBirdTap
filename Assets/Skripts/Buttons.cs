e73beb85324e9a87344a8933d12e7e4fa989e4c7
Timur <54122011+tkachikaev@users.noreply.github.com>
2020-03-04 13:48:14 +0300

    13.47

---
+++ Assets/Skripts/Buttons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    public Sprite buttonPress, buttonUnPress;
+    public bool soundButton;
+
+    private Image image;
+    private Transform child;
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+    }
+
+    void OnMouseDown()
+    {
+        image.sprite = buttonPress;
+    }
+
+    void OnMouseUp()
+    {
+        image.sprite = buttonUnPress;
+    }
+}
